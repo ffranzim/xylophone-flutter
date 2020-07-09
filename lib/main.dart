0edc630d8bfdb7976d4a0f95078a4e349c3f5fa7
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2020-07-09 17:43:28 -0300

    Secao 9 - Aula 82 - Iniciando audios

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: FlatButton(
+              child: Text('Click me!!!'),
+              onPressed: () {
+                player.play('note7.wav');
+//                player.play('assets/note1.wav');
+              },
+            ),
+          ),
         ),
       ),
     );
