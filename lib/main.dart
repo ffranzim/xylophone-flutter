4a0845633d3c5c4cc4a5154c816803ffd4e2a818
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2020-07-10 14:16:54 -0300

    Secao 9 - Aula 86

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,34 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              child: Text('Click me!!!'),
-              onPressed: () {
-                player.play('note7.wav');
-//                player.play('assets/note1.wav');
-              },
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildButton(soundNumber: 1, color: Colors.red),
+              buildButton(soundNumber: 2, color: Colors.orange),
+              buildButton(soundNumber: 3, color: Colors.yellow),
+              buildButton(soundNumber: 4, color: Colors.green),
+              buildButton(soundNumber: 5, color: Colors.teal),
+              buildButton(soundNumber: 6, color: Colors.blue),
+              buildButton(soundNumber: 7, color: Colors.purple),
+            ],
           ),
         ),
+      ),
+    );
+  }
+
+  Expanded buildButton(
+      {@required int soundNumber, @required MaterialColor color}) {
+    return Expanded(
+      flex: 1,
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          player.play('note$soundNumber.wav');
+        },
       ),
     );
   }
